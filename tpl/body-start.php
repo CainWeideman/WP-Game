271ee97aba6d179b1d97aa92dc3302c8f1dfdfd6
robinelting <r.elting.1@student.rug.nl>
2020-06-07 13:58:16 +0200

    Edited file structure body-start.php Author: Robin Elting - S3830470
    Files:
    body-start.php
    styles.css

--- tpl/body-start.php
+++ tpl/body-start.php
@@ -1,26 +1,25 @@
 <body>
 <div class="container">
-    <div class="row">
-        <div class="column">
-            <h1>Tic-Tac-Toe</h1>
-        </div>
-    </div>
-    <div class="row-status-bar">
-        <div class="status col-md-4">
-            <h3>Player X</h3>
-            <p>Wins:</p>
-            <p>Losses:</p>
-            <p>Draws:</p>
-        </div>
-        <div class="col-md-4">
-            <h4>It is the turn of (PLAYERNAME)</h4>
-        </div>
-        <div class="status col-md-4">
-            <h3>Player O</h3>
-            <p>Wins:</p>
-            <p>Losses:</p>
-            <p>Draws:</p>
+    <div class="row header">
+        <h1>Tic-Tac-Toe</h1>
+        <div class="status-bar">
+            <div class="status col-md-4">
+                <h3>Player X</h3>
+                <p class="wins">Wins:</p>
+                <p class="losses">Losses:</p>
+                <p class="draws">Draws:</p>
+            </div>
+            <div class="col-md-4">
+                <h4>It is the turn of (PLAYERNAME)</h4>
+            </div>
+            <div class="status col-md-4">
+                <h3>Player O</h3>
+                <p class="wins">Wins:</p>
+                <p class="losses">Losses:</p>
+                <p class="draws">Draws:</p>
+            </div>
         </div>
     </div>
         </div>
         <div class="col-md-12">
+            <div class="row">
