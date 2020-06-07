993735ec40438b6788a974e560322c733cd36609
ThijsBrekhof <thijs.brekhof@gmail.com>
2020-06-07 13:05:48 +0200

    added jquery bootstrap and JS links to head.php

--- tpl/head.php
+++ tpl/head.php
@@ -1,6 +1,17 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<!DOCTYPE html>
+<html lang="en">
 <head>
+    <!-- Meta Data -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title><?= $page_title ?></title>
-    <meta http-equiv="Content-Type" content="text/html;charset=utf-8" >
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="css/styles.css">
+    <link rel="script" href="scripts/main.js">
+    <!-- Scripts -->
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+
 </head>
+<body>
