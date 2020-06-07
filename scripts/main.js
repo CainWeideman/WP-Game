1c6b4b6ab38661bf5ea0b447ea1976bebecc3f6c
robinelting <r.elting.1@student.rug.nl>
2020-06-07 15:02:52 +0200

    Added function draw_cross in main.js Author: Robin Elting - S3830470
    Files:
    main.js

--- scripts/main.js
+++ scripts/main.js
@@ -12,8 +12,20 @@ function draw_circle() {
     })
 }
 
+function draw_cross() {
+    let cross = document.createElement('DIV');
+    let X = document.createElement('SPAN');
+    $(X).text('X');
+    $(X).addClass('X-mark');
+    $(cross).append(X);
+    $('.square').click(function () {
+        $(this).append(cross);
+    })
+}
+
 $(function(){
     change_page();
-    draw_circle();
+    //draw_circle();
+    draw_cross();
 });
 
