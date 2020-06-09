a6d1b8655c5a446276d9525dcbf90e17c9df4ac4
CainWeideman <c.weideman@student.rug.nl>
2020-06-09 14:53:18 +0200

    Added switch turn in main.js

--- scripts/main.js
+++ scripts/main.js
@@ -4,32 +4,40 @@ function change_page() {
     })
 }
 
+var playerturn = 'X';
+
 function draw_circle() {
-    let circle = document.createElement('DIV');
-    let O = document.createElement('SPAN');
-    $(O).text('O');
-    $(O).attr('class', 'X-mark');
-    $(circle).append(O);
-    $('.square').click(function () {
-        $(this).append(circle);
-    })
+    if (playerturn === 'O') {
+        let circle = document.createElement('DIV');
+        let O = document.createElement('SPAN');
+        $(O).text('O');
+        $(O).attr('class', 'O-mark');
+        $(circle).append(O);
+        $('.square').click(function () {
+            $(this).append(circle);
+            playerturn = 'X';
+        })
+    }
 }
 
 function draw_cross() {
-    let cross = document.createElement('DIV');
-    let X = document.createElement('SPAN');
-    $(X).text('X');
-    $(X).attr('class', 'X-mark');
-    $(cross).append(X);
-    $('.square').click(function () {
-        $(this).append(cross);
-    })
+    if (playerturn === 'X'){
+        let cross = document.createElement('DIV');
+        let X = document.createElement('SPAN');
+        $(X).text('X');
+        $(X).attr('class', 'X-mark');
+        $(cross).append(X);
+        $('.square').click(function () {
+            $(this).append(cross);
+            playerturn = 'O';
+        })
+    }
 }
 
 
 $(function(){
     change_page();
     draw_circle();
-    //draw_cross();
+    draw_cross();
 });
 
